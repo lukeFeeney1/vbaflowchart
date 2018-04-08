05cded54568636b7d15f6ceb43d71be1dfcfd744
lukeFeeney1 <luke.feeney.1@gmail.com>
2018-04-08 12:38:19 +0100

    change proj directory layout

---
+++ vbaflowchart/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""Creates a flow chart showing the flow of the routines etc for a given VBA project"""
+
+from vbaflowchart import metadata
+
+
+__version__ = metadata.version
+__author__ = metadata.authors[0]
+__license__ = metadata.license
+__copyright__ = metadata.copyright
---
+++ vbaflowchart/metadata.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Project metadata
+
+Information describing the project.
+"""
+
+# The package name, which is also the "UNIX name" for the project.
+package = 'vbaflowchart'
+project = "vbaflowchart"
+project_no_spaces = project.replace(' ', '')
+version = '0.1'
+description = 'Creates a flow chart showing the flow of the routines etc for a given VBA project'
+authors = ['Luke Feeney']
+authors_string = ', '.join(authors)
+emails = ['foobar@example.com']
+license = 'MIT'
+copyright = '2018 ' + authors_string
+url = 'http://example.com/'

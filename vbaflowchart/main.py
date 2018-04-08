c2722c0b0ba75bc04dcb2ce575c47c7e9cd5c267
lukeFeeney1 <luke.feeney.1@gmail.com>
2018-04-08 13:16:40 +0100

    clean up template

--- vbaflowchart/main.py
+++ vbaflowchart/main.py
@@ -4,31 +4,32 @@
 
 from win32com.client import Dispatch
 
-wbpath = 'C:\\Users\\Later\\Desktop\\Book1.xlsm'
-xl = Dispatch("Excel.Application")
-xl.Visible = 0
-xlwb = xl.Workbooks.Open(wbpath)
+def main(argv):
+    wbpath = 'C:\\Users\\Later\\Desktop\\Book1.xlsm'
+    xl = Dispatch("Excel.Application")
+    xl.Visible = 0
+    xlwb = xl.Workbooks.Open(wbpath)
 
-try:    
-    for i in xlwb.VBProject.VBComponents:        
-        code_mod = xlwb.VBProject.VBComponents(i.Name).CodeModule
-        #proc_kind = xlwb.VBProject.VBComponents.vbext_ProcKind
-        line_num = code_mod.CountOfDeclarationLines + 1
-        count_of_lines = code_mod.CountOfLines
-        print('----------------')
-        print(i.name)
-        print('----------------')
-        while(line_num < count_of_lines):
-            proc_name = code_mod.ProcOfLine(line_num, 0)            
-            print (proc_name[0])
-            line_num = code_mod.ProcStartLine(proc_name[0], 0) + code_mod.ProcCountLines(proc_name[0], 0) + 1
+    try:    
+        for i in xlwb.VBProject.VBComponents:        
+            code_mod = xlwb.VBProject.VBComponents(i.Name).CodeModule
+            #proc_kind = xlwb.VBProject.VBComponents.vbext_ProcKind
+            line_num = code_mod.CountOfDeclarationLines + 1
+            count_of_lines = code_mod.CountOfLines
+            print('----------------')
+            print(i.name)
+            print('----------------')
+            while(line_num < count_of_lines):
+                proc_name = code_mod.ProcOfLine(line_num, 0)            
+                print (proc_name[0])
+                line_num = code_mod.ProcStartLine(proc_name[0], 0) + code_mod.ProcCountLines(proc_name[0], 0) + 1
 
-except Exception as e:
-    print(e)
+    except Exception as e:
+        print(e)
 
-finally:    
+    finally:    
 
-    xlwb.Close(True)
-    xl.Quit
+        xlwb.Close(True)
+        xl.Quit
 
-    xl = None
+        xl = None
